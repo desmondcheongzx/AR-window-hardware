9ebde585b70575c27a38382c2a16c8636a92c3b4
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2020-12-12 22:50:41 -0500

    Update demo comments

--- dual_screen_demo.py
+++ dual_screen_demo.py
@@ -1,13 +1,17 @@
 import cv2
 from multi_screen import MultiScreen
 
+# Declare the input sources for webcameras, and the windows' output dimensions
 multiscreen = MultiScreen([0, 2], [(160, 120), (160, 120)])
 multiscreen.start_all()
 
 while True:
+    # Capture a still image from each webcam
+    # The return value will be a list of numpy matrices containing image info
     frames = multiscreen.get_frames()
     multiscreen.set_frames(frames)
 
+    # Exit the program
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
