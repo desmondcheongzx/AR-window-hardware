f6c9ec07a859770572d5d9b87f2db5dd9690acc3
arwin <arwin>
2020-12-16 07:46:36 +0000

    Resize webcam images to fill the screen

--- multi_screen.py
+++ multi_screen.py
@@ -1,6 +1,6 @@
 import cv2
+import threading
 from video_capture import VideoCaptureAsync
-#from multi_capture import VideoCaptureAsync
 
 
 class MultiScreen():
@@ -8,9 +8,7 @@ class MultiScreen():
         self.n_cameras = len(screen_dimensions)
         self.screen_dimensions = screen_dimensions
         self.video_captures = [
-            VideoCaptureAsync(src=idx,
-                              width=screen_width,
-                              height=screen_height)
+            VideoCaptureAsync(src=idx)
             for idx, (screen_width, screen_height)
             in zip(sources, screen_dimensions)]
         self.window_ids = [f'screen{i}'
@@ -20,7 +18,7 @@ class MultiScreen():
         for i, window_id in enumerate(self.window_ids):
             cv2.namedWindow(window_id, cv2.WND_PROP_FULLSCREEN)
             cv2.moveWindow(window_id, x_displacement, 0)
-            cv2.setWindowProperty(window_id, cv2.WND_PROP_FULLSCREEN, 0)
+            cv2.setWindowProperty(window_id, cv2.WND_PROP_FULLSCREEN, 1)
             x_displacement += self.screen_dimensions[i][0]
 
     def start_all(self):
@@ -34,13 +32,19 @@ class MultiScreen():
         return [capture.read()[1] for capture in self.video_captures]
 
     def set_frames(self, data, all_sources=True, src=None):
-        # Remember to check that there's enough data sources!
         if not all_sources:
             for frame, idx in zip(data, src):
-                cv2.imshow(self.window_ids[idx], frame)
+                (screen_width, 
+                    screen_height) = self.screen_dimensions[idx]
+                resized_frame = cv2.resize(
+                    frame, (screen_width, screen_height))
+                cv2.imshow(self.window_ids[idx], resized_frame)
             return
-        for frame, window_id in zip(data, self.window_ids):
-            cv2.imshow(window_id, frame)
+        for frame, (width, height), window_id in zip(
+            data, self.screen_dimensions, self.window_ids):
+            resized_frame = cv2.resize(
+                frame, (width, height))
+            cv2.imshow(window_id, resized_frame)
 
     def stop(self):
         for capture in self.video_captures:
